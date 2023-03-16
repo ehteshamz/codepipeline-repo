e9aadd7682da867d04c8f919ac1b068ca57e798c
Arham Saeed <saeedarham100@gmail.com>
2023-03-17 01:46:42 +0500

    3

--- scripts/userdata.sh
+++ scripts/userdata.sh
@@ -13,15 +13,19 @@ yum -y install wget
 
 # Downloading and Installing NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-. ~/.nvm/nvm.sh
 
 # Adding the nvm environmental variable to path
-export NVM_DIR="$HOME/.nvm"	
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
 # Installing Node
 nvm install 16
+
+# Re-adding the nvm environmental variable for the ec2-user account
+cat <<EOF >> /home/ec2-user/.bashrc
+export NVM_DIR="/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+EOF
 
 # Installing the CodeDeploy agent
 cd /home/ec2-user
